db0f9a9554d357a0025e442d8e8c77dbd44e6cf8
Arunodaya_9027 <arunodaya9027p.sg@gmail.com>
2023-05-20 15:53:19 +0530

    interpolation

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  firstName: String = 'Master';
+  lastName: String = 'Yoda';
 
+  num1: number = 10;
+  num2: number = 20;
+
+  trainingName: String = 'Angular';
+  trainingDuration: number = 10;
+
+  constructor() {}
+
+  ngOnInit() {}
 }
