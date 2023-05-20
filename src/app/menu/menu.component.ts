b49d9faba1083b4cab92e248a3977a849ad917a4
Arunodaya_9027 <arunodaya9027p.sg@gmail.com>
2023-05-20 16:58:24 +0530

    eventBinding

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -20,8 +20,23 @@ export class MenuComponent implements OnInit {
     'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==';
 
   disableMyButton: boolean = false;
+  placeholder: String = 'Enter your name';
 
   constructor() {}
+
+  sayHello() {
+    alert('Hello');
+  }
+
+  log(ev: any) {
+    console.log(ev.target.value);
+  }
+
+  change() {
+    if (this.placeholder != 'Enter your name')
+      this.placeholder = 'Enter your name';
+    else this.placeholder = this.firstName;
+  }
 
   ngOnInit() {}
 }
