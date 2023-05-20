31d8e1b817fa131fc4680ff448cf675b79620d96
Arunodaya_9027 <arunodaya9027p.sg@gmail.com>
2023-05-20 21:20:33 +0530

    ngClass-ngStyle

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -68,6 +68,13 @@ export class MenuComponent implements OnInit {
 
   constructor() {}
 
+  getTextSize(person: any) {
+    if (person.country == 'IN') return '50px';
+    else if (person.country == 'US') return '40px';
+    else if (person.country == 'UK') return '30px';
+    else return '20px';
+  }
+
   sayHello() {
     alert('Hello');
   }
